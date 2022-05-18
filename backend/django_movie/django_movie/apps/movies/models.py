1ade0759b6ac95e904c9278a61b205be76e91697
Vanya-Karalev <vankaralev@mail.ru>
2022-05-18 21:52:04 +0300

    link front and back

--- backend/django_movie/django_movie/apps/movies/models.py
+++ backend/django_movie/django_movie/apps/movies/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """Фильм"""
+    title = models.CharField("Название", max_length=100)
+    tagline = models.CharField("Слоган", max_length=100, default='')
+    description = models.TextField("Описание")
+    # poster = models.ImageField("Постер", upload_to="movies/")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2019)
+    country = models.CharField("Страна", max_length=30)
+    # directors = models.ManyToManyField(Actor, verbose_name="режиссер", related_name="film_director")
+    # actors = models.ManyToManyField(Actor, verbose_name="актеры", related_name="film_actor")
+    # genres = models.ManyToManyField(Genre, verbose_name="жанры")
+    # world_premiere = models.DateField("Примьера в мире", default=date.today)
+    # budget = models.PositiveIntegerField("Бюджет", default=0,
+    #                                      help_text="указывать сумму в долларах")
+    # fees_in_usa = models.PositiveIntegerField(
+    #     "Сборы в США", default=0, help_text="указывать сумму в долларах"
+    # )
+    # fess_in_world = models.PositiveIntegerField(
+    #     "Сборы в мире", default=0, help_text="указывать сумму в долларах"
+    # )
+    # category = models.ForeignKey(
+    #     Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True
+    # )
+    # url = models.SlugField(max_length=130, unique=True)
+    # draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    author_name = models.CharField('имя автоpа', max_length=50)
+    comment_text = models.CharField('текст комментария', max_length=200)
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
--- backend/django_movie/django_movie/settings.py
+++ backend/django_movie/django_movie/settings.py
@@ -15,6 +15,7 @@ import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 PROJECT_ROOT = os.path.dirname(__file__)
@@ -35,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movies.apps.MoviesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,7 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
